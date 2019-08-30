7ebaffb1fa8fc0c60321ba098c913b8d1976ac18
Stephen Blais <stephen@s-blais.com>
2019-08-30 20:46:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,11 @@ class Song
     @@all
   end
 
-  def new_by_filename
-
+  def new_by_filename (filename)
+    new_name = filename.split(" - ")[1]
+    song = Song.new (new_name)
+    self.artist = filename.split(" - ")[2]
+    song
   end
 
 end
