4e3239587c532f5db8ef3961b9873724196ec9d5
Stephen Blais <stephen@s-blais.com>
2019-08-30 20:05:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   end
 
   def self.find_or_create_by_name (name)
-    self.artist.name  ||= artist = Artist.new (name)
+    self.all[name] ||= artist = Artist.new (name)
   end
 
 end
