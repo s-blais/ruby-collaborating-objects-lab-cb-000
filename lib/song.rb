b8b7caee8e99eeb6f35c1f7870d2e3922ec7a1f0
Stephen Blais <stephen@s-blais.com>
2019-08-30 20:28:12 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class song
+
+  attr_accessor :name :artist
+
+  @@all = []
+
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_by_filename
+
+  end
+
+end
