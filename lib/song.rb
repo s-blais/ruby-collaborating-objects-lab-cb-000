41ad1434e24b2526ec685157098af417b0451e95
Stephen Blais <stephen@s-blais.com>
2019-08-30 22:56:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,9 @@ class Song
     song
   end
 
-    def artist_name=(name)
-      self.artist = Artist.find_or_create_by_name(name)
-      artist.add_song(self)
-    end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
 end
