01e687073875461d6d83962dd7a884c94338157f
Stephen Blais <stephen@s-blais.com>
2019-08-30 21:49:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,12 +13,12 @@ class Song
     @@all
   end
 
-  def self.new_by_filename(filename)
-      artist, song = filename.split(" - ")
-      new_song = self.new(song)
-      new_song.artist_name = artist
-      new_song
-    end
+  def self.new_by_filename (filename)
+    split_artist = filename.split(" - ")[0]
+    song = self.new(filename.split(" - ")[1])
+    new_song.artist_name = split_artist
+    song
+  end
 
     def artist_name=(name)
       self.artist = Artist.find_or_create_by_name(name)
